f6ffdeae2a149a5bc274e0d0cd1419ae497005b3
JuanTRD <tranhoan10091995@gmail.com>
2024-05-24 00:00:14 +0700

    hoan tao du an

---
+++ src/main/java/com/hlh/hlhmodule4thuno/HlhModule4ThunoApplication.java
@@ -0,0 +1,13 @@
+package com.hlh.hlhmodule4thuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlhModule4ThunoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlhModule4ThunoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hlh/hlhmodule4thuno/HlhModule4ThunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlh.hlhmodule4thuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlhModule4ThunoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
